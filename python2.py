841118addd1fd84e4ef25b86acca5683304726a9
danilaple <denlex76@gmail.com>
2022-03-27 16:29:52 +0300

    Добавление вводимых значений и условий

--- python2.py
+++ python2.py
@@ -1,9 +1,9 @@
 print ("I can add up the numbers and compare with yours")
-a = int(input("Введите число 1: "))
-b = int(input("Введите число 2: "))
-c = int(input("Введите ваш ответ: "))
+a = int(input("Enter the number 1: "))
+b = int(input("Enter the number 2: "))
+c = int(input("Enter your answer: "))
 sum = a + b 
 if c == sum:
     print ("Correctly")
 elif c != sum:
-    print ("неверно")
+    print ("Wrong")
